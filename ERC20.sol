7cb5c5efe28c6cbe146652eb530760457f170daf
Andhika Aristia <wootekhina@gmail.com>
2024-08-24 22:02:17 +0700

    ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -1,167 +1,315 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.17;
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v5.0.0) (token/ERC20/ERC20.sol)
 
-// Importing OpenZeppelin contracts for ERC20 and EnumerableSet functionalities
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+pragma solidity ^0.8.20;
 
-// Contract for weighted voting using ERC20 token
-contract WeightedVoting is ERC20 {
-    string private salt = "itsmyryb"; // A private string variable
-    using EnumerableSet for EnumerableSet.AddressSet; // Importing EnumerableSet for address set functionality
+import {IERC20} from "./IERC20.sol";
+import {IERC20Metadata} from "./extensions/IERC20Metadata.sol";
+import {Context} from "../../utils/Context.sol";
+import {IERC20Errors} from "../../interfaces/draft-IERC6093.sol";
 
-    // Custom errors
-    error TokensClaimed(); // Error for attempting to claim tokens again
-    error AllTokensClaimed(); // Error for attempting to claim tokens when all are already claimed
-    error NoTokensHeld(); // Error for attempting to perform an action without holding tokens
-    error QuorumTooHigh(); // Error for setting a quorum higher than total supply
-    error AlreadyVoted(); // Error for attempting to vote more than once
-    error VotingClosed(); // Error for attempting to vote on a closed issue
+/**
+ * @dev Implementation of the {IERC20} interface.
+ *
+ * This implementation is agnostic to the way tokens are created. This means
+ * that a supply mechanism has to be added in a derived contract using {_mint}.
+ *
+ * TIP: For a detailed writeup see our guide
+ * https://forum.openzeppelin.com/t/how-to-implement-erc20-supply-mechanisms/226[How
+ * to implement supply mechanisms].
+ *
+ * The default value of {decimals} is 18. To change this, you should override
+ * this function so it returns a different value.
+ *
+ * We have followed general OpenZeppelin Contracts guidelines: functions revert
+ * instead returning `false` on failure. This behavior is nonetheless
+ * conventional and does not conflict with the expectations of ERC20
+ * applications.
+ *
+ * Additionally, an {Approval} event is emitted on calls to {transferFrom}.
+ * This allows applications to reconstruct the allowance for all accounts just
+ * by listening to said events. Other implementations of the EIP may not emit
+ * these events, as it isn't required by the specification.
+ */
+abstract contract ERC20 is Context, IERC20, IERC20Metadata, IERC20Errors {
+    mapping(address account => uint256) private _balances;
 
-    // Struct to represent an issue
-    struct Issue {
-        EnumerableSet.AddressSet voters; // Set of voters
-        string issueDesc; // Description of the issue
-        uint256 quorum; // Quorum required to close the issue
-        uint256 totalVotes; // Total number of votes casted
-        uint256 votesFor; // Total number of votes in favor
-        uint256 votesAgainst; // Total number of votes against
-        uint256 votesAbstain; // Total number of abstained votes
-        bool passed; // Flag indicating if the issue passed
-        bool closed; // Flag indicating if the issue is closed
+    mapping(address account => mapping(address spender => uint256)) private _allowances;
+
+    uint256 private _totalSupply;
+
+    string private _name;
+    string private _symbol;
+
+    /**
+     * @dev Sets the values for {name} and {symbol}.
+     *
+     * All two of these values are immutable: they can only be set once during
+     * construction.
+     */
+    constructor(string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
     }
 
-    // Struct to represent a serialized issue
-    struct SerializedIssue {
-        address[] voters; // Array of voters
-        string issueDesc; // Description of the issue
-        uint256 quorum; // Quorum required to close the issue
-        uint256 totalVotes; // Total number of votes casted
-        uint256 votesFor; // Total number of votes in favor
-        uint256 votesAgainst; // Total number of votes against
-        uint256 votesAbstain; // Total number of abstained votes
-        bool passed; // Flag indicating if the issue passed
-        bool closed; // Flag indicating if the issue is closed
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() public view virtual returns (string memory) {
+        return _name;
     }
 
-    // Enum to represent different vote options
-    enum Vote {
-        AGAINST,
-        FOR,
-        ABSTAIN
+    /**
+     * @dev Returns the symbol of the token, usually a shorter version of the
+     * name.
+     */
+    function symbol() public view virtual returns (string memory) {
+        return _symbol;
     }
 
-    // Array to store all issues
-    Issue[] internal issues;
-
-    // Mapping to track if tokens are claimed by an address
-    mapping(address => bool) public tokensClaimed;
-
-    uint256 public maxSupply = 1000000; // Maximum supply of tokens
-    uint256 public claimAmount = 100; // Amount of tokens to be claimed
-
-    string saltt = "any"; // Another string variable
-
-    // Constructor to initialize ERC20 token with a name and symbol
-    constructor(string memory _name, string memory _symbol)
-        ERC20(_name, _symbol)
-    {
-        issues.push(); // Pushing an empty issue to start from index 1
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5.05` (`505 / 10 ** 2`).
+     *
+     * Tokens usually opt for a value of 18, imitating the relationship between
+     * Ether and Wei. This is the default value returned by this function, unless
+     * it's overridden.
+     *
+     * NOTE: This information is only used for _display_ purposes: it in
+     * no way affects any of the arithmetic of the contract, including
+     * {IERC20-balanceOf} and {IERC20-transfer}.
+     */
+    function decimals() public view virtual returns (uint8) {
+        return 18;
     }
 
-    // Function to claim tokens
-    function claim() public {
-        // Check if all tokens have been claimed
-        if (totalSupply() + claimAmount > maxSupply) {
-            revert AllTokensClaimed();
-        }
-        // Check if the caller has already claimed tokens
-        if (tokensClaimed[msg.sender]) {
-            revert TokensClaimed();
-        }
-        // Mint tokens to the caller
-        _mint(msg.sender, claimAmount);
-        tokensClaimed[msg.sender] = true; // Mark tokens as claimed
+    /**
+     * @dev See {IERC20-totalSupply}.
+     */
+    function totalSupply() public view virtual returns (uint256) {
+        return _totalSupply;
     }
 
-    // Function to create a new voting issue
-    function createIssue(string calldata _issueDesc, uint256 _quorum)
-        external
-        returns (uint256)
-    {
-        // Check if the caller holds any tokens
-        if (balanceOf(msg.sender) == 0) {
-            revert NoTokensHeld();
-        }
-        // Check if the specified quorum is higher than total supply
-        if (_quorum > totalSupply()) {
-            revert QuorumTooHigh();
-        }
-        // Create a new issue and return its index
-        Issue storage _issue = issues.push();
-        _issue.issueDesc = _issueDesc;
-        _issue.quorum = _quorum;
-        return issues.length - 1;
+    /**
+     * @dev See {IERC20-balanceOf}.
+     */
+    function balanceOf(address account) public view virtual returns (uint256) {
+        return _balances[account];
     }
 
-    // Function to get details of a voting issue
-    function getIssue(uint256 _issueId)
-        external
-        view
-        returns (SerializedIssue memory)
-    {
-        Issue storage _issue = issues[_issueId];
-        return
-            SerializedIssue({
-                voters: _issue.voters.values(),
-                issueDesc: _issue.issueDesc,
-                quorum: _issue.quorum,
-                totalVotes: _issue.totalVotes,
-                votesFor: _issue.votesFor,
-                votesAgainst: _issue.votesAgainst,
-                votesAbstain: _issue.votesAbstain,
-                passed: _issue.passed,
-                closed: _issue.closed
-            });
+    /**
+     * @dev See {IERC20-transfer}.
+     *
+     * Requirements:
+     *
+     * - `to` cannot be the zero address.
+     * - the caller must have a balance of at least `value`.
+     */
+    function transfer(address to, uint256 value) public virtual returns (bool) {
+        address owner = _msgSender();
+        _transfer(owner, to, value);
+        return true;
     }
 
-    // Function to cast a vote on a voting issue
-    function vote(uint256 _issueId, Vote _vote) public {
-        Issue storage _issue = issues[_issueId];
+    /**
+     * @dev See {IERC20-allowance}.
+     */
+    function allowance(address owner, address spender) public view virtual returns (uint256) {
+        return _allowances[owner][spender];
+    }
 
-        // Check if the issue is closed
-        if (_issue.closed) {
-            revert VotingClosed();
-        }
-        // Check if the caller has already voted
-        if (_issue.voters.contains(msg.sender)) {
-            revert AlreadyVoted();
-        }
+    /**
+     * @dev See {IERC20-approve}.
+     *
+     * NOTE: If `value` is the maximum `uint256`, the allowance is not updated on
+     * `transferFrom`. This is semantically equivalent to an infinite approval.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     */
+    function approve(address spender, uint256 value) public virtual returns (bool) {
+        address owner = _msgSender();
+        _approve(owner, spender, value);
+        return true;
+    }
 
-        uint256 nTokens = balanceOf(msg.sender);
-        // Check if the caller holds any tokens
-        if (nTokens == 0) {
-            revert NoTokensHeld();
-        }
+    /**
+     * @dev See {IERC20-transferFrom}.
+     *
+     * Emits an {Approval} event indicating the updated allowance. This is not
+     * required by the EIP. See the note at the beginning of {ERC20}.
+     *
+     * NOTE: Does not update the allowance if the current allowance
+     * is the maximum `uint256`.
+     *
+     * Requirements:
+     *
+     * - `from` and `to` cannot be the zero address.
+     * - `from` must have a balance of at least `value`.
+     * - the caller must have allowance for ``from``'s tokens of at least
+     * `value`.
+     */
+    function transferFrom(address from, address to, uint256 value) public virtual returns (bool) {
+        address spender = _msgSender();
+        _spendAllowance(from, spender, value);
+        _transfer(from, to, value);
+        return true;
+    }
 
-        // Update vote counts based on the vote option
-        if (_vote == Vote.AGAINST) {
-            _issue.votesAgainst += nTokens;
-        } else if (_vote == Vote.FOR) {
-            _issue.votesFor += nTokens;
+    /**
+     * @dev Moves a `value` amount of tokens from `from` to `to`.
+     *
+     * This internal function is equivalent to {transfer}, and can be used to
+     * e.g. implement automatic token fees, slashing mechanisms, etc.
+     *
+     * Emits a {Transfer} event.
+     *
+     * NOTE: This function is not virtual, {_update} should be overridden instead.
+     */
+    function _transfer(address from, address to, uint256 value) internal {
+        if (from == address(0)) {
+            revert ERC20InvalidSender(address(0));
+        }
+        if (to == address(0)) {
+            revert ERC20InvalidReceiver(address(0));
+        }
+        _update(from, to, value);
+    }
+
+    /**
+     * @dev Transfers a `value` amount of tokens from `from` to `to`, or alternatively mints (or burns) if `from`
+     * (or `to`) is the zero address. All customizations to transfers, mints, and burns should be done by overriding
+     * this function.
+     *
+     * Emits a {Transfer} event.
+     */
+    function _update(address from, address to, uint256 value) internal virtual {
+        if (from == address(0)) {
+            // Overflow check required: The rest of the code assumes that totalSupply never overflows
+            _totalSupply += value;
         } else {
-            _issue.votesAbstain += nTokens;
+            uint256 fromBalance = _balances[from];
+            if (fromBalance < value) {
+                revert ERC20InsufficientBalance(from, fromBalance, value);
+            }
+            unchecked {
+                // Overflow not possible: value <= fromBalance <= totalSupply.
+                _balances[from] = fromBalance - value;
+            }
         }
 
-        // Add the caller to the list of voters and update total votes count
-        _issue.voters.add(msg.sender);
-        _issue.totalVotes += nTokens;
+        if (to == address(0)) {
+            unchecked {
+                // Overflow not possible: value <= totalSupply or value <= fromBalance <= totalSupply.
+                _totalSupply -= value;
+            }
+        } else {
+            unchecked {
+                // Overflow not possible: balance + value is at most totalSupply, which we know fits into a uint256.
+                _balances[to] += value;
+            }
+        }
 
-        // Close the issue if quorum is reached and determine if it passed
-        if (_issue.totalVotes >= _issue.quorum) {
-            _issue.closed = true;
-            if (_issue.votesFor > _issue.votesAgainst) {
-                _issue.passed = true;
+        emit Transfer(from, to, value);
+    }
+
+    /**
+     * @dev Creates a `value` amount of tokens and assigns them to `account`, by transferring it from address(0).
+     * Relies on the `_update` mechanism
+     *
+     * Emits a {Transfer} event with `from` set to the zero address.
+     *
+     * NOTE: This function is not virtual, {_update} should be overridden instead.
+     */
+    function _mint(address account, uint256 value) internal {
+        if (account == address(0)) {
+            revert ERC20InvalidReceiver(address(0));
+        }
+        _update(address(0), account, value);
+    }
+
+    /**
+     * @dev Destroys a `value` amount of tokens from `account`, lowering the total supply.
+     * Relies on the `_update` mechanism.
+     *
+     * Emits a {Transfer} event with `to` set to the zero address.
+     *
+     * NOTE: This function is not virtual, {_update} should be overridden instead
+     */
+    function _burn(address account, uint256 value) internal {
+        if (account == address(0)) {
+            revert ERC20InvalidSender(address(0));
+        }
+        _update(account, address(0), value);
+    }
+
+    /**
+     * @dev Sets `value` as the allowance of `spender` over the `owner` s tokens.
+     *
+     * This internal function is equivalent to `approve`, and can be used to
+     * e.g. set automatic allowances for certain subsystems, etc.
+     *
+     * Emits an {Approval} event.
+     *
+     * Requirements:
+     *
+     * - `owner` cannot be the zero address.
+     * - `spender` cannot be the zero address.
+     *
+     * Overrides to this logic should be done to the variant with an additional `bool emitEvent` argument.
+     */
+    function _approve(address owner, address spender, uint256 value) internal {
+        _approve(owner, spender, value, true);
+    }
+
+    /**
+     * @dev Variant of {_approve} with an optional flag to enable or disable the {Approval} event.
+     *
+     * By default (when calling {_approve}) the flag is set to true. On the other hand, approval changes made by
+     * `_spendAllowance` during the `transferFrom` operation set the flag to false. This saves gas by not emitting any
+     * `Approval` event during `transferFrom` operations.
+     *
+     * Anyone who wishes to continue emitting `Approval` events on the`transferFrom` operation can force the flag to
+     * true using the following override:
+     * ```
+     * function _approve(address owner, address spender, uint256 value, bool) internal virtual override {
+     *     super._approve(owner, spender, value, true);
+     * }
+     * ```
+     *
+     * Requirements are the same as {_approve}.
+     */
+    function _approve(address owner, address spender, uint256 value, bool emitEvent) internal virtual {
+        if (owner == address(0)) {
+            revert ERC20InvalidApprover(address(0));
+        }
+        if (spender == address(0)) {
+            revert ERC20InvalidSpender(address(0));
+        }
+        _allowances[owner][spender] = value;
+        if (emitEvent) {
+            emit Approval(owner, spender, value);
+        }
+    }
+
+    /**
+     * @dev Updates `owner` s allowance for `spender` based on spent `value`.
+     *
+     * Does not update the allowance value in case of infinite allowance.
+     * Revert if not enough allowance is available.
+     *
+     * Does not emit an {Approval} event.
+     */
+    function _spendAllowance(address owner, address spender, uint256 value) internal virtual {
+        uint256 currentAllowance = allowance(owner, spender);
+        if (currentAllowance != type(uint256).max) {
+            if (currentAllowance < value) {
+                revert ERC20InsufficientAllowance(spender, currentAllowance, value);
+            }
+            unchecked {
+                _approve(owner, spender, currentAllowance - value, false);
             }
         }
     }
